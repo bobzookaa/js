bfecfcd5809667f7fd8b7c63e319d6c85586b90f
bobzookaa <30707667+bobzookaa@users.noreply.github.com>
2017-08-04 08:26:00 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="common-problems-playing-games.html"}
 if(x=="2"){xxx="4-reasons-to-love-being-pc-gamer.html"}
 if(x=="3"){xxx="a-new-season-in-gaming.html"}
-if(x=="4"){xxx="top-reasons-to-love-pc-games.html"}a_to_ve[i].href="http://protectedlinks.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="top-reasons-to-love-pc-games.html"}a_to_ve[i].href="https://protectedlinks.blogspot.com/2017/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
