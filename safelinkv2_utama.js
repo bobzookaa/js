0ddbc0db81424d0167fbc35bdfe3b7aa74a534ad
bobzookaa <30707667+bobzookaa@users.noreply.github.com>
2017-08-04 08:39:19 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="common-problems-playing-games.html"}
-if(x=="2"){xxx="4-reasons-to-love-being-pc-gamer.html"}
-if(x=="3"){xxx="a-new-season-in-gaming.html"}
-if(x=="4"){xxx="top-reasons-to-love-pc-games.html"}a_to_ve[i].href="https://protectedlinks.blogspot.com/2017/08/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="top-reasons-to-love-pc-games.html"}
+if(x=="2"){xxx="a-new-season-in-gaming.html"}
+if(x=="3"){xxx="4-reasons-to-love-being-pc-gamer.html"}
+if(x=="4"){xxx="common-problems-playing-games.html"}a_to_ve[i].href="https://protectedlinks.blogspot.com/2017/08/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
